628360f17d0580e7fb8ecf12eb469a3e61ab97a7
h.osipenko <h.osipenko@hqsoftwarelab.com>
2021-11-09 14:21:13 +0300

    Send and check token functions are added; All data is relocated to DB

--- routes/servers.js
+++ routes/servers.js
@@ -1,12 +1,31 @@
 import { Router } from 'express';
 import { getAll, getFullData } from '../controllers/pokemons.js';
 import { loginUser, registerUser } from '../controllers/users.js';
+import jwt from 'jsonwebtoken'
 
 const router = Router()
 
-router.get('/api/pokemon-list', getAll)
-router.get('/api/pokemons:id', getFullData)
+const verifyJwt = (req, res, next) => {
+    const token = req.headers["authorization"]
+    if(!token){
+        res.status(498).end()
+    }else{
+        jwt.verify(token, "jwtSecret", (err) => {
+           if (err){
+               console.log(token);
+               res.status(498).end()
+           } else {
+                next()
+           }
+        })
+    }
+}
+
+router.get('/api/pokemon-list', verifyJwt, getAll)
+router.get('/api/pokemons:id', verifyJwt, getFullData)
 router.post('/api/registration', registerUser)
 router.post('/api/login', loginUser)
+
+
 
 export default router
